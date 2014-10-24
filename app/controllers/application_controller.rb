c6cb7eb964687b22acb5be58171ca2a79052400a
Peter Ching <peter.ching@gmail.com>
2014-10-23 20:52:42 -0400

    Set application#gut_check as root.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def gut_check
+    render text: "Gut check OK!"
+  end
 end
